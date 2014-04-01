7a3fd26608f3965c875ca1400213f88205b79899
Wang Yibin <wyb7@qq.com>
2014-04-01 09:04:12 +0800

    add comment for highlight current line

--- appearance.el
+++ appearance.el
@@ -1,6 +1,8 @@
-
+;; Turn on hl-line:
 (global-hl-line-mode 1)
+;; Set any color as the background face of the current line:
 (set-face-background 'hl-line "#3e4446")
+;; To keep syntax highlighting in the current line:
 (set-face-foreground 'highlight nil)
 
 ;; Highlight matching parentheses when the point is on them.
